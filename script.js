c567cb79318e66b6010cead33d6d756c2bc2df97
Virginie Dourson <virginiedourson@gmail.com>
2022-12-01 10:27:39 +0100

    Liste

---
+++ script.js
@@ -0,0 +1,109 @@
+const collection = [
+    {
+        name : "Legend",
+        group : "Mono",
+        album : "For my parents",
+        releaseYear : 2012,
+        style : [
+            "neoclassical",
+            "post rock",
+        ],
+        picture : "./assets/mono.jpg",
+
+    },
+    {
+        name : "Lonely Day",
+        group : "System Of A down",
+        album : "Hypnotize",
+        releaseYear: 2005,
+        style : [
+            "heavy metal",
+            "hard rock",
+        ],
+        picture : "./assets/lonely_day.png",
+    },
+    {
+        name : "Quand je marche",
+        group : "Ben Mazué",
+        album : "Paradis",
+        releaseYear: 2020,
+        style : [
+            "Chanson française",
+        ],
+        picture : "./assets/lonely_day.png",
+    },
+    {
+        name : "You Don't Know",
+        group : "Milow",
+        album : "Milow",
+        releaseYear: 2009,
+        style : [
+            "Acoustic",
+            "Pop rock",
+        ],
+        picture : "./assets/lonely_day.png",
+    },
+    {
+        name : "Verona",
+        group : "Muse",
+        album : "Will of the people",
+        releaseYear: 2022,
+        style : [
+            "Alternative Rock",
+        ],
+        picture : "./assets/lonely_day.png",
+    },
+    {
+        name : "A Letter To Elise",
+        group : "The Cure",
+        album : "Wish",
+        releaseYear: 1992,
+        style : [
+           "Alternative Rock",
+           "New Wave",
+           "Pop Rock",
+        ],
+        picture : "./assets/cure.png",
+    },
+    {
+        name : "Guinea Pig",
+        group : "Girls in Hawai",
+        album : "Nocturne",
+        releaseYear: 2017,
+        style : [
+            "Ballad",
+        ],
+        picture : "./assets/girls_in_hawai.png",
+    },
+    {
+        name : "Mirror",
+        group : "Loic Nottet",
+        album : "Selfocracy",
+        releaseYear: 2017,
+        style : [
+            "Indie Pop",
+        ],
+        picture : "./assets/nottet.png",
+    },
+    {
+        name : "Easy Come Easy Go",
+        group : "Alice On The Roof",
+        album : "Higher",
+        releaseYear: 2016,
+        style : [
+            "Synth-pop",
+        ],
+        picture : "./assets/alice.png",
+    },
+    {
+        name : "Conjouring",
+        group : "IAMDDB",
+        album : "Hoodrich Vol.3",
+        releaseYear: 2017,
+        style : [
+            "Trap",
+        ],
+        picture : "./assets/iamddb.png",
+    },
+   
+]
